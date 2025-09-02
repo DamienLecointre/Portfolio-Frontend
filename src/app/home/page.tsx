3d5405c0b3160f14bdbc45e99d5d68395009aec7
Damien Lecointre <damien.lecointre75@gmail.com>
2025-09-02 17:40:55 +0200

    added fullScreenSize mixin + added Home style file

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,11 @@
+import styles from "@/styles/pages/Home.module.scss";
+
 function HomePage() {
   return (
-    <div>
+    <div className={styles.pageContainer}>
       <h1>HOME</h1>
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
